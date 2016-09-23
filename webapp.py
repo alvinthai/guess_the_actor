a548acce2d87b2fec56f8c43bfdd6f11b3058e5b
Alvin <alvinthai@gmail.com>
2016-09-23 00:11:24 -0700

    now supports cookies

--- webapp.py
+++ webapp.py
@@ -1,61 +1,63 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, session
 import networkx as nx
 import numpy as np
+import os
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.urandom(24)   # random cookie
 
 imdb = nx.read_edgelist('data/imdb_edges_clean.tsv', delimiter='\t')
 movies = np.loadtxt('data/movies_with_difficulty.tsv', dtype=str,
                     delimiter='\t')
 
 length = movies.shape[0]
-result = 'correct'
-correct, total = 0, 0
 
 
 @app.route('/')
 def home():
-    global total, correct
-    total = 0
-    correct = 0
+    session['total'] = 0
+    session['correct'] = 0
     return render_template('home.html')
 
 
 @app.route('/play', methods=['POST'])
 def play():
-    global movie1, movie2
-    difficulty = int(str(request.form['difficulty']))
+    session['difficulty'] = int(str(request.form['difficulty']))
 
-    movie1, movie2, movie_difficulty = movies[np.random.randint(length)]
+    session['movie1'], session['movie2'], session['movie_difficulty'] = \
+        movies[np.random.randint(length)]
     # re-randomize if movie pair is too difficult
-    while int(movie_difficulty) > difficulty:
-        movie1, movie2, movie_difficulty = movies[np.random.randint(length)]
-    return render_template('play.html', movie1=movie1, movie2=movie2,
-                           difficulty=difficulty)
+    while int(session['movie_difficulty']) > session['difficulty']:
+        session['movie1'], session['movie2'], session['movie_difficulty'] = \
+            movies[np.random.randint(length)]
+    return render_template('play.html', difficulty=session['difficulty'],
+                           movie1=session['movie1'], movie2=session['movie2'])
 
 
 @app.route('/results', methods=['POST'])
 def results():
-    global movie1, movie2
-    difficulty = int(str(request.form['difficulty']))
+    y_pos = int(str(request.form['y_pos']))
+    border_height = int(str(request.form['border_height']))
 
-    answers = set(imdb.neighbors(movie1)) & set(imdb.neighbors(movie2))
+    answers = set(imdb.neighbors(session['movie1'])) & \
+        set(imdb.neighbors(session['movie2']))
     good = [ans.lower() for ans in answers]
 
-    global total, correct
-    total += 1
+    session['total'] += 1
     submission = str(request.form['actor_name'].encode('utf-8'))
-    answers_html = '<br>'.join(answers)
+    answers_html = ', '.join(answers)
 
     if submission.lower() in good:
-        correct += 1
+        session['correct'] += 1
         result = 'correct'
     else:
         result = 'incorrect'
 
     return render_template('results.html', submission=submission,
-                           result=result, correct=correct, total=total,
-                           answers_html=answers_html, difficulty=difficulty)
+                           result=result, correct=session['correct'],
+                           total=session['total'], answers_html=answers_html,
+                           difficulty=session['difficulty'],
+                           y_pos=y_pos, border_height=border_height)
 
 
 if __name__ == '__main__':
